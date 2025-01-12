fe5139d152c65db41a936376669b2aa25a2422d7
Space <143919787+Rhcs2023@users.noreply.github.com>
2025-01-11 21:42:16 -0600

    Update P9.py

--- P9.py
+++ P9.py
@@ -4,16 +4,17 @@ import tempfile
 
 # Diccionario con palabras a traducir
 diccionario = {
- #   'a': 'uno ',
-  #  'b': 'dos ',
-   # 'c': 'tres ',
-  #  'd': 'cuatro ',
-  #  'e': 'cinco ',
-  #  'f': 'seis ',
-  #  'g': 'siete ',
-   # 'mi': 'ri ',
-  #  'tuyo': 'tu ',
-  #  'mío': 'mi ',
+    # Agrega aquí tus traducciones
+    # 'a': 'uno ',
+    # 'b': 'dos ',
+    # 'c': 'tres ',
+    # 'd': 'cuatro ',
+    # 'e': 'cinco ',
+    # 'f': 'seis ',
+    # 'g': 'siete ',
+    # 'mi': 'ri ',
+    # 'tuyo': 'tu ',
+    # 'mío': 'mi ',
 }
 
 def traducir_oracion(oracion):
@@ -36,16 +37,17 @@ if 'oracion_traducida' not in st.session_state:
     st.session_state.oracion_traducida = ""
 
 # Opción para introducir texto
-oracion_usuario = st.text_input("Introduce una oración, presiona enter:")
+oracion_usuario = st.text_area("Introduce una oración (máx. 10 líneas):", height=200)
 
-# Traducir la oración ingresada por el usuario
-if oracion_usuario:
-    oracion_traducida = traducir_oracion(oracion_usuario)
-    st.session_state.oracion_traducida = oracion_traducida
-    st.write(f"Traducción: {oracion_traducida}")
-    audio_bytes = reproducir_audio(oracion_traducida, 'es')  # Usando español por defecto
-    st.audio(audio_bytes, format='audio/mp3')
-    
+# Botón para ejecutar la traducción
+if st.button("Traducir"):
+    if oracion_usuario:
+        oracion_traducida = traducir_oracion(oracion_usuario)
+        st.session_state.oracion_traducida = oracion_traducida
+        audio_bytes = reproducir_audio(oracion_traducida, 'es')  # Usando español por defecto
+        st.audio(audio_bytes, format='audio/mp3')
+    else:
+        st.warning("Por favor, introduce una oración antes de traducir.")
 
 
 
